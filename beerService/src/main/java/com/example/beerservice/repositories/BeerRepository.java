0f5acd8c1bacdf31b23fad2e5fb0b4eb5a660778
kawshik <sadnan.kibria.kawshik@dohatec.com.bd>
2023-04-09 14:29:31 +0600

    Beer Repository Added

---
+++ beerService/src/main/java/com/example/beerservice/repositories/BeerRepository.java
@@ -0,0 +1,10 @@
+package com.example.beerservice.repositories;
+
+import com.example.beerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+}
